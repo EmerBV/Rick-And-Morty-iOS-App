cf8b9c9bee30069dd412ff9e6fcad0bfa276e9dc
EmerBV <emersmc@hotmail.com>
2023-01-02 18:34:11 +0100

    Initial Commit

---
+++ Rick And Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick And Morty
+//
+//  Created by Emerson Balahan Varona on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
